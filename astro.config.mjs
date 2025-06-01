201511e29649074bd796b6bfad0df5206c10d4fd
Ratchata Tanakornwongsarat <gosdaykungth@gmail.com>
2025-06-01 10:55:44 +0700

    change to astro framework

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://Arttie747.github.io',
+  base: 'My-webpage',
+});
